e87cec39000ee6f94e48922f88694c8de8d846fc
jalal cheff <jlalalmyahy571@gmail.com>
2023-07-11 20:38:22 +0300

    add PizzaPager

---
+++ app/src/main/java/com/example/pizzaorderapp/compasible/PizzaPager.kt
@@ -0,0 +1,41 @@
+package com.example.pizzaorderapp.compasible
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pizzaorderapp.R
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PizzaPager(){
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        HorizontalPager(
+            pageCount = 5,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.bread_1),
+                contentDescription = "bread",
+                modifier = Modifier.size(96.dp)
+            )
+        }
+    }
+
+}
+
+@Composable
+@Preview(widthDp = 360 , heightDp = 800)
+fun PreviewPizzaPager(){
+    PizzaPager()
+}
