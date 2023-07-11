f6861af7974d1c1208df87f1e12afe6fe6a22e9e
jalal cheff <jlalalmyahy571@gmail.com>
2023-07-11 20:40:03 +0300

    add HorizontalSpacer and VerticalSpacer

---
+++ app/src/main/java/com/example/pizzaorderapp/compasible/HorizontalSpacer.kt
@@ -0,0 +1,13 @@
+package com.example.pizzaorderapp.compasible
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalSpacer(space:Int){
+    Spacer(modifier = Modifier.width(space.dp))
+}
